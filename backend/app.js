8132f0f090084baf4be3dd6fb5065cc53aebd26b
nocout5 <eli_kmus@hotmail.fr>
2022-07-22 14:47:47 +0200

    start again

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,9 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+
 const app = express();
-app.use(express.json());
-const stuffRoutes = require('./routes/stuff');
-const userRoutes = require('./routes/user');
-const path = require('path');
-
-
-mongoose
-  .connect(
-    "mongodb+srv://jean_OC:OC_password@cluster0.m3gusco.mongodb.net/?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => console.log("Connexion à MongoDB réussie !"))
-  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
-app.use('/api/sauces', stuffRoutes);
-app.use('/api/auth', userRoutes);
-app.use('/images', express.static(path.join(__dirname, 'images')));
+
 
 module.exports = app;
