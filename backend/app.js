2be13819806cf3c2ba3d96d5a56d705d0d8deea7
nocout5 <eli_kmus@hotmail.fr>
2022-07-23 18:49:23 +0200

    add auth + routes post and get image visible on frontend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,28 @@
-const express = require('express');
-
+const express = require("express");
+const User = require("./models/User");
+const mongoose = require('mongoose')
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce')
+const path = require('path');
 const app = express();
+app.use(express.json());
 
+mongoose
+  .connect(
+    "mongodb+srv://jean_OC:OC_password@cluster0.m3gusco.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
-
-
+app.use('/api/auth', userRoutes);
+app.use('/api/sauces', sauceRoutes)
+app.use('/images', express.static(path.join(__dirname, 'images')))
 module.exports = app;
---
+++ backend/controllers/sauce.js
@@ -0,0 +1,34 @@
+const Sauce = require("../models/Sauce");
+
+exports.getAllSauces = (req, res, next) => {
+  Sauce.find()
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({_id: req.params.id})
+  .then(sauce => res.status(200).json(sauce))
+  .catch(error => res.status(404).json(error))
+}
+
+exports.createSauce = (req, res, next) => {
+  const thingObject = JSON.parse(req.body.sauce);
+  delete thingObject._id;
+  const sauce = new Sauce({
+    ...thingObject,
+    userId: req.auth.userId,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
+  });
+  console.log(sauce)
+  sauce
+    .save()
+    .then(() => {
+      res.status(201).json({ message: "Objet enregistré !" });
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
+};
